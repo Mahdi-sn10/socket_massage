6f2439ddf9cc2208bb42c3041d51f607782e4859
Mahdi-sn10 <xboy.pleyer10@gmail.com>
2024-11-09 13:02:51 +0330

    Update socket_cl.py
    add while

--- socket_cl.py
+++ socket_cl.py
@@ -4,8 +4,8 @@ HOST = "127.0.0.1"
 PORT = 8001
 with  socket.socket(socket.AF_INET,socket.SOCK_STREAM) as s:
     s.connect((HOST,PORT))
-    massage = input("type any thin:  ")
-    s.sendall(bytes(massage,encoding='utf8'))
-    data = s.recv(1024)
-    
-print(f"Receve {data.decode("utf-8")}")
+    while True:
+        massage = input("type any thin:  ")
+        s.sendall(bytes(massage,encoding='utf8'))
+        data = s.recv(1024)
+        print(f"Receve {data.decode("utf-8")}")
